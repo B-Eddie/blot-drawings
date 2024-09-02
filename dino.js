4270fcb50002c45337047930084abd6fb627089e
Eddie <121364681+B-Eddie@users.noreply.github.com>
2024-09-02 18:37:11 -0400

    Update dino.js

--- dino.js
+++ dino.js
@@ -117,11 +117,98 @@ be.down();
 be.right(127)
 be.arc(360, 1.5)
 
+// for (let i = 0; i<4; i++) {
+//   be.pos = [65 - i, 81.5 - i]
+//   be.up();
+//   be.down();
+//   be.right(127)
+//   be.arc(360, 1.5 - i*0.1)
+// }
+be.pos = [61, 83.4]
+be.up();
+be.down();
+turnMove(be, "left", 289, 4)
+
+be.pos = [48.5, 85.5]
+be.up();
+be.down();
+turnMove(be, "left", 286, 4)
+
+be.pos = [47.4, 76.7]
+be.up();
+be.down();
+turnMove(be, "left", 49, 4)
+
+be.right(3)
+be.arc(-65, 2.5)
+be.right(-103)
+be.arc(23, 3.0)
+be.right(26)
+be.arc(-65, 2.5)
+be.right(44)
+be.arc(20, 5.0)
+be.right(-100)
+be.arc(-71, 4.0)
+be.right(0)
+be.arc(-71, 4.0)
+be.right(17)
+be.arc(-71, 4.4)
+be.right(-4)
+be.arc(-45, 7.1)
+be.right(-71)
+be.arc(-45, 10.0)
+be.right(10)
+be.arc(-117, 3.6)
+
+// belly
+be.pos = [47.3, 48.3]
+be.up();
+be.down();
+be.right(154)
+be.arc(208, 12.2)
+be.right(0)
+be.arc(76, 14.4)
+be.right(-16)
+be.arc(65, 13.8)
+
+be.pos = [56.0, 44.3]
+be.up();
+be.down();
+turnMove(be, "left", 59, 4)
+be.pos = [56.0, 41.8]
+be.up();
+be.down();
+turnMove(be, "left", 92, 4)
+
+const bl = new bt.Turtle()
+// cheeks
+bl.pos = [66, 76] 
+bl.up()
+bl.down()
+bl.right(43)
+bl.arc(105, 5)
+turnMove(bl, "left", 38, 2)
+bl.right(-40)
+bl.arc(103, 5)
+turnMove(bl, "left", 34, 2)
+
+bl.pos = [35, 76] 
+bl.up()
+bl.down()
+bl.right(-32)
+bl.arc(100, 5)
+turnMove(bl, "left", 36, 2)
+bl.right(-40)
+bl.arc(103, 5)
+turnMove(bl, "left", 37, 2.1)
+
+
 bodyOutline.push(...bo.lines())
 bodyBack.push(...bb.lines())
 bodyFace.push(...be.lines())
+bodyBelly.push(...bl.lines())
 
-body.push(...bodyOutline,  ...bodyBack, ...bodyFace)
+body.push(...bodyOutline,  ...bodyBack, ...bodyFace, ...bodyBelly)
 
 const final = [...body, ...back, ...exclaim, ...arms]
 drawLines(final)
